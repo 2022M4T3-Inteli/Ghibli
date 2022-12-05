7d953989bedcbf40949183d7d0a65d236c265f7c
Rezende00 <pedro.rezende@sou.inteli.edu.br>
2022-12-05 00:14:30 -0300

    Sprint 4 - Implementação dos Códigos Responders
    Adição dos Códigos Responders (FTM) dos Beacons:
    - Circuito --> Sprint 4
    - Web --> micro_data --> ESP32S3

---
+++ src/Web/micro_data/ESP32S3/BEACONS_ESP32S3/RESPONDER_BEACON3/RESPONDER_BEACON3.ino
@@ -0,0 +1,19 @@
+/* Wi-Fi FTM Responder Arduino Example
+  This example code is in the Public Domain (or CC0 licensed, at your option.)
+  Unless required by applicable law or agreed to in writing, this
+  software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+  CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include "WiFi.h"
+// SSID e senha Wifi para fazer a conexao entre a o beacon e tag
+const char * WIFI_FTM_SSID = "baconzitos3"; // Trocar para "beaconghibli3"
+const char * WIFI_FTM_PASS = "QazWsx@123";
+void setup(){
+  Serial.println("Iniciando o SoftAp com suporte ao FTM Responder");
+  // Habilitação do AP com suporte ao FTM
+  WiFi.softAP(WIFI_FTM_SSID, WIFI_FTM_PASS, 1, 0, 4, true);
+}
+
+void loop() {
+  delay(1000);
+}
